5e189029383cae6bec18f58b53d33a65e344dde5
Sergey Kazakov <kazaimazai@gmail.com>
2021-06-08 14:38:58 +0300

    implemented login state recuder

--- PureduxDemo/Core/StateComponents/Login/PickCredentials.swift
+++ PureduxDemo/Core/StateComponents/Login/PickCredentials.swift
@@ -10,12 +10,17 @@ import PureduxCommonCore
 
 extension LoginState {
     struct PickCredentials: Codable {
+
         private(set) var username: String = ""
         private(set) var password: String = ""
 
         private(set) var requesToken: RequestState<SingleRequest> = .none
-        private(set) var createSession: RequestState<SingleRequest> = .none
+        private(set) var createSession: RequestState<SessionRequest> = .none
     }
+}
+
+extension LoginState.PickCredentials {
+    typealias SessionRequest = RequestWithPayload<SingleRequest, SessionRequestToken>
 }
 
 extension LoginState.PickCredentials {
@@ -32,6 +37,87 @@ extension LoginState.PickCredentials {
 
 extension LoginState.PickCredentials {
     mutating func reduce(_ action: Action, env: AppEnvironment) {
+        switch action {
+        case is Actions.Auth.Login.Flow:
+            self = .init()
 
+        case let action as Actions.Auth.Login.UpdateUsername:
+            updateUsername(value: action.value)
+
+        case let action as Actions.Auth.Login.UpdatePassword:
+            updatePassword(value: action.value)
+
+        case is Actions.Auth.Login.Perform:
+            performLoginIfPossible(with: env.makeUUID())
+
+        case let action as Actions.Auth.ObtainToken.Result.Success:
+            setRequesTokenSuccess()
+            createSessionWith(token: action.token, requestId:  env.makeUUID())
+
+        case is Actions.Auth.ObtainToken.Result.Failed:
+            setRequesTokenFailed()
+
+        case is Actions.Auth.Login.Result.Success:
+            setCreateSessionSuccess()
+
+        case is Actions.Auth.Login.Result.Failed:
+            setCreateSessionFailed()
+        default:
+            break
+        }
+    }
+}
+
+// MARK: - Mutations
+
+private extension LoginState.PickCredentials {
+    mutating func updateUsername(value: String) {
+        if isInProgress {
+            return
+        }
+
+        username = value
+    }
+
+    mutating func updatePassword(value: String) {
+        if isInProgress {
+            return
+        }
+
+        password = value
+    }
+
+    mutating func performLoginIfPossible(with requestId: UUID) {
+        guard canPerformLogin else {
+            return
+        }
+
+        if isInProgress {
+            return
+        }
+
+        requesToken = .inProgress(SingleRequest(id: RequestID(rawValue: requestId)))
+    }
+
+    mutating func setRequesTokenFailed() {
+        requesToken = .failed
+    }
+
+    mutating func setRequesTokenSuccess() {
+        requesToken = .success
+    }
+
+    mutating func createSessionWith(token: SessionRequestToken, requestId: UUID) {
+        createSession = .inProgress(SessionRequest(
+            payload: token,
+            state: SingleRequest(id: RequestID(rawValue: requestId))))
+    }
+
+    mutating func setCreateSessionFailed() {
+        createSession = .failed
+    }
+
+    mutating func setCreateSessionSuccess() {
+        createSession = .success
     }
 }
